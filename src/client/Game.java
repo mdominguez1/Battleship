31a259dcf03506a5ff4066920201f6389ca62ab7
tpruett23 <vdpruett1@catamount.wcu.edu>
2017-11-15 15:36:15 -0500

    Add files via upload

--- src/client/Game.java
+++ src/client/Game.java
@@ -1,32 +1,50 @@
 package client;
 
 import java.util.ArrayList;
+
+import java.util.List;
+
+import java.util.Collections;
+import java.util.Collection;
+
+
+
 /**
  * Holds all the methods to perform a game of BattleShip.
  * @author Victoria Pruett & Melchor Dominguez
  * @version 11/13/17
  *
  */
+
 public class Game {
+
 	/**
 	 * Arraylist that holds all of the players.
 	 */
-	ArrayList<Player> playerList = new ArrayList<Player>();
-	
+	List<Player> playerList;
 	/**
 	 * Boolean to tell if a game is already in progress.
 	 */
+	boolean inProgress;
 
-	boolean inProgress = false;
-	
 	/**
 	 * The play method checks to see if there is a already a game in progress
 	 * and if there are enough players and then starts a game accordingly.	
 	 */
+
+	public Game(){
+		
+		playerList = new ArrayList<Player>();
+		inProgress = false;
+		
+	}
+
+
 	public void play() {
 		if(inProgress = false) {
 			if(playerList.size() >= 2) {
 				System.out.println("The game begins!");
+				inProgress = true;
 
 			}else {
 				System.out.println("Not enough players to play the game");
@@ -37,72 +55,81 @@ public class Game {
 
 		}
 	}
-	
 	/**
 	 * Adds a player to the game if the username has not already been used and if 
 	 * there is not already a game in progress.
 	 * @param username The username of the player that wants to join the game.
 	 */
-	public void join(String username) {
-		Player newPlayer = new Player(username);
-		if(!playerList.contains(newPlayer)){
-		playerList.add(newPlayer);
-		System.out.println("!!! " + username + " has joined the game");
-	}else{
-		System.out.println("Player has already joined the game.");
+
+	public void join(Player player) {
 		
-	    }
+		if(!playerList.contains(player)){
+			playerList.add(player);
+			System.out.println("!!! " + player.getUsername() + " " + " has joined the game");
+		}else{
+			System.out.println("That user has already joined the game.");
+
+		}
+	}
+/*public void Print(){
+	for(int i = 0; i < playerList.size(); i++){
+		System.out.println(playerList.get(i).username + " ");
 	}
 	
+	
+}
+*/
 	
 	/**
 	 * Decides whose turn it is next.
 	 */
 	public void turn(){
-		// Still working on this method.
-		for(int i = 0; i < playerList.size(); i++){
-			System.out.println(playerList.get(i).getUsername() + " it is your turn");
-			
-		}
+		
+		//for(int i = 0; i < playerList.size(); i++){
+			System.out.println("It is " + playerList.get(0).username + "'s" + " turn!");
+
+		//}
+
+		Collections.rotate(playerList, 1);
+
+
 	}
-	
-	
-	
-/**
- * Holds all the information to create a new player. Also contains 
- * methods to get and receive appropriate fields.
- * @author Victoria Pruett & Melchor Dominguez
- * @version 11/13/17
- * 
- */
-	public class Player{
+
+
+
+	/**
+	 * Holds all the information to create a new player. Also contains 
+	 * methods to get and receive appropriate fields.
+	 * @author Victoria Pruett & Melchor Dominguez
+	 * @version 11/13/17
+	 * 
+	 */
+	public  class Player{
+		
+
 		/**
-		* The username of the player.
-		*/
+		 * The username of the player.
+		 */
 		private String username;
-		
+
 		/**
-		* The players grid or game board.
-		*/
+		 * The players grid or game board.
+		 */
 		private Grid playerGrid;
-		
-		
-		
+
 		/**
 		 * Constructor to give a player a username.
 		 * @param username Username of the player.
 		 */
 		public Player(String username) {
-			// Still working on this check.
-			for(int i = 0; i < playerList.size(); i++)
-				if(!playerList.get(i).username.equals(username)){
+			
 					this.username = username;
-				} else{
-					System.out.println("Please choose a different username. "
-						+ "The one you have chosen has already been taken.");
-			}	
-		}
+				    playerGrid = new Grid(4);
+				    
+			}
+
 		
+
 		/**
 		 * Setter method to set or change the players username.
 		 * @param username Username of the player.
@@ -110,7 +137,7 @@ public class Game {
 		public void setUsername(String username) {
 			this.username = username;
 		}
-		
+
 		/**
 		 * Getter method to get the players username.
 		 * @return The players username.
@@ -119,7 +146,29 @@ public class Game {
 			return this.username;
 		}
 	}
+	public static void main(String args[]) {
 		
+	Player play = new Game().new Player("player1");
+	Player newPlayer = new Game().new Player("player2");
+	Player thirdPlayer = new Game().new Player("player3");
+	
+	Game game = new Game();
+	game.join(play);
+	game.join(newPlayer);
+	game.join(thirdPlayer);
+	game.join(thirdPlayer);
+	//game.Print();
+	game.turn();
+	game.turn();
+	game.turn();
+   game.play();
+   play.playerGrid.printGrid();
+   
+   
+		
+	
+	}
+
 }
 
 
